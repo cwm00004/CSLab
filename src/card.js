cee03b53b73fe139c4c79c7bc381bfcb60508b06
CWM00004 <cwm00004@mix.wvu.edu>
2023-10-27 15:29:57 -0400

    bootstrapcode#1

--- src/card.js
+++ src/card.js
@@ -1,14 +1,8 @@
-// Card.js
-
-import React from 'react';
-
-function Card() {
-  return (
-    <div className="card">
-      <h3>Card Title</h3>
-      <p>This is a card description.</p>
-    </div>
-  );
-}
-
-export default Card;
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap"></img>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
