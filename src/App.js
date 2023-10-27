769146b008d4576b3ffe7ea0f36f67d6964236b3
CWM00004 <cwm00004@mix.wvu.edu>
2023-10-26 20:56:38 -0400

    attempt 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
+// App.js
+
+import React from 'react';
 import './App.css';
+import Navbar from './Navbar'; // Import the Navbar component
+import Card from './card'; // Import the Card component
 
 function App() {
   return (
     <div className="App">
-       <h1>CS 230L</h1>
-        <h2>Section - 03</h2>
-        <p>WVU ID: 800333892</p>
-        <p>Hi, I am Corbin</p>
+      <Navbar /> {/* Use the Navbar component */}
+      <h1>CS 230L</h1>
+      <h2>Section - 03</h2>
+      <p>WVU ID: 800333892</p>
+      <p>Hi, I am Corbin</p>
+      <div className="card-container">
+        <Card /> {/* Use the Card component multiple times for each card */}
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
---
+++ src/card.js
@@ -0,0 +1,14 @@
+// Card.js
+
+import React from 'react';
+
+function Card() {
+  return (
+    <div className="card">
+      <h3>Card Title</h3>
+      <p>This is a card description.</p>
+    </div>
+  );
+}
+
+export default Card;
