3d22ee9ca8a920d5e361c746a8d1398992d88d20
CWM00004 <cwm00004@mix.wvu.edu>
2023-10-28 13:35:54 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@
 
 import React from 'react';
 import './App.css';
-import Navbar from './Navbar'; // Import the Navbar component
-import Card from './card'; // Import the Card component
+import Navbar from './Navbar';
+import Card from './card';
 
 function App() {
   return (
     <div className="App">
-      <Navbar /> {/* Use the Navbar component */}
+      <Navbar/>
       <h1>CS 230L</h1>
       <h2>Section - 03</h2>
       <p>WVU ID: 800333892</p>
       <p>Hi, I am Corbin</p>
       <div className="card-container">
-        <Card /> {/* Use the Card component multiple times for each card */}
+        <Card /> 
         <Card />
         <Card />
       </div>
