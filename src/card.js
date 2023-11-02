4563589f61ba591ade631a72d9a7ab3252c27ca3
CWM00004 <cwm00004@mix.wvu.edu>
2023-11-02 16:09:54 -0400

    huwfuk.bkjbv

--- src/card.js
+++ src/card.js
@@ -1,26 +1,16 @@
 import React from 'react';
 
-const Card = ({ backgroundColor }) => {
-  const cardStyle = {
-    width: '18rem',
-  };
-
-  const imgStyle = {
-    /* Define styles for your image here, if needed */
-  };
-
-  const cardBackgroundClass = `bg-${backgroundColor}`;
-
+const card = () => {
   return (
-    <div className={`card ${cardBackgroundClass}`} style={cardStyle}>
-      <img src="..." className="card-img-top" alt="..." style={imgStyle} />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="..."></img>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
   );
 }
 
-export default Card;
+export default card;
