c0b0663b6dcd17f51de2a46ffb6a441379ab23ca
CWM00004 <cwm00004@mix.wvu.edu>
2023-11-02 15:39:30 -0400

    hateMe

--- src/card.js
+++ src/card.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
-const card = () => {
+const Card = () => {
+  const cardStyle = {
+    width: '18rem'
+  };
+
+  const imgStyle = {
+    /* Define styles for your image here, if needed */
+  };
+
   return (
-<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="..."></img>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+    <div className="card" style={cardStyle}>
+      <img src="..." className="card-img-top" alt="..." style={imgStyle} />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
   );
 }
 
-export default card;
+export default Card;
