8e2d1c27b70d741c13445571f7e58fde958a8dfb
CWM00004 <cwm00004@mix.wvu.edu>
2023-11-02 16:03:22 -0400

    whyistheworlssocruel

--- src/card.js
+++ src/card.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ backgroundColor }) => {
   const cardStyle = {
-    width: '18rem'
+    width: '18rem',
   };
 
   const imgStyle = {
     /* Define styles for your image here, if needed */
   };
 
+  const cardBackgroundClass = `bg-${backgroundColor}`;
+
   return (
-    <div className="card" style={cardStyle}>
+    <div className={`card ${cardBackgroundClass}`} style={cardStyle}>
       <img src="..." className="card-img-top" alt="..." style={imgStyle} />
       <div className="card-body">
         <h5 className="card-title">Card title</h5>
