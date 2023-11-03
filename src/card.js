e1d4fcde0afb096fcefee51a72fe84e724711007
CWM00004 <cwm00004@mix.wvu.edu>
2023-11-03 09:33:06 -0400

    work

--- src/card.js
+++ src/card.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Card from './card';
 
-const card = () => {
+function App() {
   return (
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="..."></img>
+  <img class="card-img-top" src="..." alt="Card image cap"></img>
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -13,4 +14,4 @@ const card = () => {
   );
 }
 
-export default card;
+export default App;
